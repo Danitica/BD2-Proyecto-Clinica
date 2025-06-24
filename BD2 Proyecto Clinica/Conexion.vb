944d3070a421baacfa25dced2e28f97c6088d651
Danitica1 <danicorderos27@gmail.com>
2025-06-23 22:49:24 -0600

    Agregar archivos de proyecto.

---
+++ BD2 Proyecto Clinica/Conexion.vb
@@ -0,0 +1,25 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Conexion
+    Private myconexion As MySqlConnection
+
+    Public Sub New()
+        Dim cadena As String = "server=localhost;user=root;password=password;database=login;"
+        myconexion = New MySqlConnection(cadena)
+    End Sub
+
+    Public Function Open() As MySqlConnection
+        If myconexion.State = ConnectionState.Closed Then
+            myconexion.Open()
+            'MessageBox.Show("Hola la conexion es exitosa")
+
+        End If
+        Return myconexion
+    End Function
+
+    Public Sub Cerrar()
+        If myconexion.State = ConnectionState.Open Then
+            myconexion.Close()
+            'MessageBox.Show("Hola la conexion se cerró correctamente")
+        End If
+    End Sub
+End Class
---
+++ BD2 Proyecto Clinica/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub lblclave_Click(sender As Object, e As EventArgs) Handles lblclave.Click
+
+    End Sub
+
+    Private Sub brnSalir_Click(sender As Object, e As EventArgs) Handles brnSalir.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btningresar_Click(sender As Object, e As EventArgs) Handles btningresar.Click
+        'Validar el nombre del usuario y la contraseña, que no este los espacios vacios' y conectar a la base de datos para comprobar que el usuario existe'
+        If String.IsNullOrWhiteSpace(txtClave.Text) Then
+            MessageBox.Show("Por favor, ingrese la clave.", "Campo vacío", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            txtClave.Focus()
+            Exit Sub
+        End If
+    End Sub
+End Class
